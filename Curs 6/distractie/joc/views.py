010de26c22a05f49d859fb5143e9ee11ef269460
Alina Serban <alina.decembrie@gmail.com>
2025-06-24 12:50:03 +0300

    Rock Paper Scissor finalizat

--- Curs 6/distractie/joc/views.py
+++ Curs 6/distractie/joc/views.py
@@ -2,6 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
+import random
+import enum
+
+class Optiuni(enum.Enum):
+    Rock = 1
+    Paper = 2
+    Scissor = 3
+
+    def __str__(self):
+        return self.name
+
+def logica_de_joc(client:int):
+    client = Optiuni(client)
+    server = Optiuni(random.choice([1, 2, 3]))
+    
+    if client == server:
+        rezultat = "Egalitate"
+    elif client == Optiuni.Rock and server == Optiuni.Scissor:
+        rezultat = "Castig"
+    elif client == Optiuni.Scissor and server == Optiuni.Paper:
+        rezultat = "Castig"
+    elif client == Optiuni.Paper and server == Optiuni.Rock:
+        rezultat = "Castig"
+    else:
+        rezultat = "Pierdere"
+        
+    return client, server, rezultat
+
 def rock_paper_view (request):
     if request.method == 'POST':
         print("Metoda -- POST")
@@ -9,12 +37,14 @@ def rock_paper_view (request):
         client = request.POST.get("chosen")
         
         #Logica de business ...
-        server = '...'
-        rezultat = '...'
+        if client and (client in ("1", "2", "3")):
+            alegere_client, alegere_server, rezultat_joc  = logica_de_joc(int(client))
     
-        context ={'client':'Varianta clientului',
-              'server':'Varianta serverului',
-              'rezultat':'Cineva a castigat'}
+            context = {'client': alegere_client,
+                        'server': alegere_server,
+                        'rezultat': rezultat_joc}
+        else:
+            context = { }    
     
     else:
         print("Metoda -- GET")
