c5c1d20aaa83c2af45120b6f33d335f3383d2882
Alina Serban <alina.decembrie@gmail.com>
2025-06-24 16:24:35 +0300

    Curs 6 finalizat

--- Curs 6/distractie/joc/urls.py
+++ Curs 6/distractie/joc/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import rock_paper_view
+from .views import rock_paper_view, rock_paper_scissor_lizzard_spock_view
 
 urlpatterns = [
-    path('', rock_paper_view),
+    path('clasic', rock_paper_view),
+    path('frumos',rock_paper_scissor_lizzard_spock_view),
 ]
